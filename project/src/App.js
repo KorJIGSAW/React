b2f76e725bfa7a2e85bf71b081ed4fb8cf532d7b
KorJIGSAW <111522731+KorJIGSAW@users.noreply.github.com>
2023-11-21 23:58:58 +0900

    게임 필요 내용 주석 추가 및 보완

--- project/src/App.js
+++ project/src/App.js
@@ -1,44 +1,52 @@
 import React, { Component } from 'react';
 
+// 사용자 정보를 관리하는 클래스
 class User {
   constructor(nickname) {
-    this.nickname = nickname;
-    this.score = 0;
+    this.nickname = nickname; // 사용자의 닉네임
+    this.score = 0; // 사용자의 점수
   }
 }
 
+// 게임을 실행하는 클래스
 class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      score: 0,
+      score: 0, // 게임 점수
+      // 여기에 게임에 필요한 상태들을 작성하면돼! 예를 들면, 점프 여부나 장애물 위치 등등? 생각나는거 작성해봥
     };
   }
 
+  //여기에는 게임 메소드를 작성하면돼 이것도 예를 들자면, 공룡이 점프하는 메소드나 장애물이 오는 속도 메소드, 장애물 충돌여부?
+
+  // 게임 화면을 출력하는 메소드
   render() {
     return (
       <div>
-        <h1>Score: {this.state.score}</h1>
+        {/* 게임 화면 출력 */}
+        <h1>Score: {this.state.score}</h1> {/* 점수 출력 */}
         <div style={{display: 'flex', justifyContent: 'space-around'}}>
-          <div style={{padding: '10px', border: '1px solid black'}}>Dino</div>
-          <div style={{padding: '10px', border: '1px solid black'}}>Obstacle</div>
+          <div style={{padding: '10px', border: '1px solid black'}}>Dino</div> {/* 공룡 표시 */}
+          <div style={{padding: '10px', border: '1px solid black'}}>Obstacle</div> {/* 장애물 표시 */}
         </div>
       </div>
     );
   }
 }
 
+// App메인 클래스
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      users: [],
-      currentUser: null,
-      signupForm: {
+      users: [], // 등록된 사용자들
+      currentUser: null, // 현재 로그인한 사용자
+      signupForm: { // 회원가입 폼의 상태
         nickname: '',
         password: ''
       },
-      loginForm: {
+      loginForm: { // 로그인 폼의 상태
         nickname: '',
         password: ''
       },
@@ -46,6 +54,7 @@ class App extends Component {
     };
   }
 
+  // 회원가입 폼의 입력을 처리하는 메소드
   handleSignupChange = (e) => {
     this.setState({ 
       signupForm: {
@@ -55,6 +64,7 @@ class App extends Component {
     });
   }
 
+  // 로그인 폼의 입력을 처리하는 메소드
   handleLoginChange = (e) => {
     this.setState({ 
       loginForm: {
@@ -64,11 +74,12 @@ class App extends Component {
     });
   }
 
+  // 회원가입을 처리하는 메소드
   handleSignup = (e) => {
     e.preventDefault();
-    const newUser = new User(this.state.signupForm.nickname);
+    const newUser = new User(this.state.signupForm.nickname); // 새 User 인스턴스 생성
     this.setState({ 
-      users: [...this.state.users, newUser],
+      users: [...this.state.users, newUser], // 새 사용자 추가
       signupForm: {
         nickname: '',
         password: ''
@@ -76,11 +87,12 @@ class App extends Component {
     });
   }
 
+  // 로그인을 처리하는 메소드
   handleLogin = (e) => {
     e.preventDefault();
-    const user = this.state.users.find((user) => user.nickname === this.state.loginForm.nickname);
+    const user = this.state.users.find((user) => user.nickname === this.state.loginForm.nickname); // 닉네임이 일치하는 사용자 찾기
     this.setState({ 
-      currentUser: user,
+      currentUser: user, // 현재 사용자 설정
       loginForm: {
         nickname: '',
         password: ''
@@ -88,20 +100,22 @@ class App extends Component {
     });
   }
 
+  // 게임 시작을 처리하는 메소드
   handleStartGame = () => {
     this.setState({ gameStarted: true }); // 게임 시작 상태 변경
   }
 
+  // 화면을 출력하는 메소드
   render() {
     return (
       <div className="App">
         {this.state.currentUser ? 
           (this.state.gameStarted ? 
-            <Game user={this.state.currentUser} /> : // 게임 시작 시 게임 화면 출력
+            <Game user={this.state.currentUser} /> : // 로그인한 경우, 게임 시작 시 게임 화면 출력
             <div> 
               <button onClick={this.handleStartGame}>게임 시작</button> {/* 게임 시작 버튼 */}
-              <button onClick={this.handleDescriptGame}>게임 설명: 장애물을 피해 점수를 획득하는 게임입니다.</button> {/* 게임 설명 */}
-              <button OnClick={this.handleRankingGame}>게임 랭킹: 랭킹 정보를 여기에 출력할 수 있습니다.</button> {/* 게임 랭킹 */}
+              <p>게임 설명: 장애물을 피해 점수를 획득하는 게임입니다.</p> {/* 게임 설명 */}
+              <p>게임 랭킹: 랭킹 정보를 여기에 출력할 수 있습니다.</p> {/* 게임 랭킹 */}
             </div>
           ) : (
             <div>
